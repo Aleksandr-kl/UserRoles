7892ed063d91cc0c788c948dfba1b0a9cfae41bc
Aleksandr-kl <kalenskuy@gmail.com>
2023-07-30 17:23:07 +0300

    added class WebConfiguration

--- src/main/java/com/example/userrole/dao/UserRepository.java
+++ src/main/java/com/example/userrole/dao/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.userrole.dao;
 import com.example.userrole.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findFirstByName(String name);
+    //User findFirstByName(String name);
+    Optional<User> findByUsername(String name);
 }
